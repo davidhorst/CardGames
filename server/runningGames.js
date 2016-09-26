17484e2c3870927bb3354652f1577cd0ecd0fc61
perkinsb01 <spencer@spencerperkins.net>
2016-09-26 12:50:03 -0700

    started very basic functions for runningGames.js

--- server/runningGames.js
+++ server/runningGames.js
@@ -1,15 +1,37 @@
 'use strict';
+//holds games and has methods to add and remove games from server
+
 class RunningGames {
     constructor() {
         this.games = {};
         //id:gameObj
     }
+    add(game_obj){
+        const gameGuid = guid();
+        this.games[gameGuid] = game_obj;
+        retur gameGuid;
 
-    //add
-    //remove
-    //get
+
+    }
+
+    remove(game_id) {
+        //code here to remove any data from users that are in the game_id
+        delete this.game[game_id];
+    }
+
+    get(game_id) {
+        return this.game[game_id];
+    }
+
+    guid() {
+        s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+      }
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+    }
 
 }
 module.exports = new RunningGames();
-
-//holds games and has methods to add and remove games from server
